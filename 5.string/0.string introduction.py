6a037a0c9b5921c913a4061a686027920b30f071
NRjadav <109718224+NRjadav@users.noreply.github.com>
2022-09-26 15:47:45 -0700

    Python

---
+++ 5.string/0.string introduction.py
@@ -0,0 +1,18 @@
+
+
+"""
+string: combonation of maltiple charactor in double quotes or single quotes
+
+"""
+
+name=input("enter a name:")
+print(name)
+
+# print(len(name))
+
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+
+
+
