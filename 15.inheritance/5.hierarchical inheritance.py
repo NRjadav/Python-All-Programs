e3ad4608482982c41e3173c8d2a493bb34e1b60e
NRjadav <109718224+NRjadav@users.noreply.github.com>
2023-02-13 12:01:45 -0800

    Python

---
+++ 15.inheritance/5.hierarchical inheritance.py
@@ -0,0 +1,25 @@
+
+class A:
+    def displayA(self):
+        print("class A")
+class B(A):
+    def displayB(self):
+        print("class B")
+class C(A):
+    def displayC(self):
+        print("class C")
+
+obj=B()
+obj.displayA()
+obj.displayB()
+obj1=C()
+obj1.displayA()
+obj1.displayC()
+
+
+
+
+
+
+
+
